425d3917f6130e3e760efe01b39cf4adeb2d6099
Eivind <eivind@linux.com>
2017-11-26 00:03:37 +0100

    Starting some actual layout changes

---
+++ src/containers/article/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+export default class Article extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render () {
+    return <div />
+  }
+}
---
+++ src/containers/gallery/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+export default class Gallery extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render () {
+    return <div />
+  }
+}
